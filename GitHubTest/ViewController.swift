a215d204b15076194587ca9f9440c3df9489d73a
Marcel Fokker <marcel1@192.168.1.7>
2017-08-24 20:39:12 +0200

    De print regel wordt uitgevoerd.

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("GitHub wordt uitgevoerd")
+        print("De regel wordt normaals uitgevoerd")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
